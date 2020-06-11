0b284161e624b6b00029922a32e70697835f6ce0
MUHAMMAD AZHAR <azharthegeek@gmail.com>
2020-06-12 02:45:24 +0500

    updated

--- src/Room.js
+++ src/Room.js
@@ -1,28 +1,25 @@
 import React, { useState } from 'react';
+import './Room.css'
 
 function Room() {
 
-    //const state = useState(true);
-    //console.log("State = ",state);
-    let [isLit, setLit] = useState(false);
-    let [age , setAge] = useState(24);
-    
+    let [isLitON, setLitON] = useState(true);
+    let [tempF , setTempF] = useState(72);
+
     return (
-    <div>        
-        This Room is {isLit? "lit": "dark"}
+    <div className={`room ${isLitON? "ON": "OFF"}`}> 
+        &nbsp;&nbsp;&nbsp;<h1>Room Light is {isLitON? "ON": "OFF"}</h1>
         <br/>
-        Age: {age}
+        &nbsp;&nbsp;&nbsp;<button className='button' onClick={()=> setLitON(true) } >ON</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;<button  className='button' onClick={()=> setLitON(false) } >OFF</button>
         <br/>
-        <button onClick={()=> setLit(!isLit) } >Toggle Light</button>
+        &nbsp;&nbsp;&nbsp;<h1>Room Current Temperature is: {tempF}</h1>
         <br/>
-        <button onClick={ ()=> {
-                        console.log("Increase age arrow function");
-                        setAge(++age);
-                        }}>
-            Increase Age
-        </button>
+        &nbsp;&nbsp;&nbsp;<button  className='button' title="Increase Temperature" onClick={()=> setTempF(++tempF)}>+</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;<button  className='button' title="Decrease Temperature" onClick={()=> setTempF(--tempF)}>-</button>
+        <h5>Boot Camp 2020 By Zia Khan & Team</h5>
+        <a href="https://github.com/AZHARTHEGEEK">Visit My GitHub Account</a>
     </div>
     );
 }
-
 export default Room;
